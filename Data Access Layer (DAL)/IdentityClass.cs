87500eef278df83e06f84fe4b3aeec100fcdfd2c
Mark-abdallah <83455664+Mark-abdallah@users.noreply.github.com>
2021-07-16 22:11:35 +0200

    layout

--- Data Access Layer (DAL)/IdentityClass.cs
+++ Data Access Layer (DAL)/IdentityClass.cs
@@ -73,10 +73,10 @@ namespace Data_Access_Layer__DAL_
 
     public class ApplicationDBContext : IdentityDbContext<AppIdentityUser>
     {
-        //@"Data Source=DESKTOP-7U1FJEF\SQLSERVERMARK;Initial Catalog='Final-eCommerce';Integrated Security=True"
         //
+        //"Data Source=.;Initial Catalog=Final-ECommerce;Integrated Security=True"
 
-        public ApplicationDBContext() : base("Data Source=.;Initial Catalog=Final-ECommerce;Integrated Security=True")
+        public ApplicationDBContext() : base(@"Data Source=DESKTOP-7U1FJEF\SQLSERVERMARK;Initial Catalog='Final-eCommerce';Integrated Security=True")
         {
 
         }
